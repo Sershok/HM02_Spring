ae891ce2a9dc2c1362e1fca787a7919ce7cf5aa7
serge <agentlukas321@gmail.com>
2022-09-06 22:22:11 +0300

    initial commit

---
+++ src/main/java/com/example/hm02spring/Hm02SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.hm02spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hm02SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hm02SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hm02spring/Hm02SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hm02spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hm02SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
